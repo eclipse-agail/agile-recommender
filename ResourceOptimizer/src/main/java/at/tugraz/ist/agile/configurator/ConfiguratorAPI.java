88ae4d73c584ed6af80931e79385fe20463c837d
sedapolaterdeniz <spolater@ist.tugraz.at>
2017-03-08 14:04:56 +0100

    getters and setters are created for Eurotech

--- ResourceOptimizer/src/main/java/at/tugraz/ist/agile/configurator/ConfiguratorAPI.java
+++ ResourceOptimizer/src/main/java/at/tugraz/ist/agile/configurator/ConfiguratorAPI.java
@@ -32,7 +32,6 @@ public class ConfiguratorAPI {
         return "Agile Configurator is running. You can access its restful post-service via /getResourceOptimization.";
     }
     
-    
     @ResponseBody @RequestMapping("/getResourceOptimization")
     public GatewayProfile getResourceOptimization(@RequestBody GatewayProfile profile) {
     	
@@ -40,6 +39,7 @@ public class ConfiguratorAPI {
     	return optimizer.getconfiguredProfile();
       
     }
+    
     @ResponseBody @RequestMapping("/getAppRecommendation")
     public RecommendedApps getAppRecommendation(@RequestBody at.tugraz.ist.agile.recommendermodels.GatewayProfile profile) {
     	
@@ -52,6 +52,7 @@ public class ConfiguratorAPI {
 		return recommendedApps;
 		
     }
+    
     @ResponseBody @RequestMapping("/getWorkflowRecommendation")
     public RecommendedWFs getWorkflowRecommendation (@RequestBody at.tugraz.ist.agile.recommendermodels.GatewayProfile profile) {
     	
@@ -63,7 +64,9 @@ public class ConfiguratorAPI {
 		return result;
       
     }
+    
     @ResponseBody @RequestMapping("/getCloudRecommendation")
+    
     public RecommendedClouds getCloudRecommendation (@RequestBody at.tugraz.ist.agile.recommendermodels.GatewayProfile profile) {
     	
     	RestTemplate restTemplate = new RestTemplate();
@@ -99,5 +102,103 @@ public class ConfiguratorAPI {
       
     }
 
+    @ResponseBody @RequestMapping("/getPluggedDevices")
+    public RecommendedDevices getPluggedDevices () {
+    	return StaticGatewayProfile.devices;
+    }
+    @ResponseBody @RequestMapping("/getinstalledApps")
+    public RecommendedApps getinstalledApps () {
+    	return StaticGatewayProfile.apps;
+    }
+    @ResponseBody @RequestMapping("/getinstalledWfs")
+    public RecommendedWFs getinstalledWfs () {
+    	return StaticGatewayProfile.wfs;
+    }
+    @ResponseBody @RequestMapping("/getResources")
+    public String getResources () {
+    	return StaticGatewayProfile.resources;
+    }
+    @ResponseBody @RequestMapping("/getLocation")
+    public String getLocation () {
+    	return StaticGatewayProfile.location;
+    }
+    @ResponseBody @RequestMapping("/getPricingPreferences")
+    public String getPricingPreferences () {
+    	return StaticGatewayProfile.pricingPreferences;
+    }
+    
+    
+    @ResponseBody @RequestMapping("/getsupportedDataEncodingProtocolsOfGateway")
+    public int[] getsupportedDataEncodingProtocolsOfGateway () {
+    	return StaticGatewayProfile.supportedDataEncodingProtocolsOfGateway;
+    }
+    @ResponseBody @RequestMapping("/getsupportedConnectivityProtocolsOfGateway")
+    public int[] getsupportedConnectivityProtocolsOfGateway () {
+    	return StaticGatewayProfile.supportedConnectivityProtocolsOfGateway;
+    }
+    @ResponseBody @RequestMapping("/getuserRequirementWeight_Performance")
+    public int getuserRequirementWeight_Performance () {
+    	return StaticGatewayProfile.userRequirementWeight_Performance;
+    }
+    @ResponseBody @RequestMapping("/getuserRequirementWeight_Reliability")
+    public int getuserRequirementWeight_Reliability () {
+    	return StaticGatewayProfile.userRequirementWeight_Reliability;
+    }
+    @ResponseBody @RequestMapping("/getuserRequirementWeight_Cost")
+    public int getuserRequirementWeight_Cost () {
+    	return StaticGatewayProfile.userRequirementWeight_Cost;
+    }
+    
    
+    @RequestMapping("/setPluggedDevices")
+    public void setPluggedDevices (@RequestBody RecommendedDevices devs) {
+    	 StaticGatewayProfile.devices = devs;
+    }
+    @RequestMapping("/setinstalledApps")
+    public void setinstalledApps (@RequestBody RecommendedApps apps) {
+    	 StaticGatewayProfile.apps = apps;
+    }
+    @RequestMapping("/setinstalledWfs")
+    public void setinstalledWfs (@RequestBody RecommendedWFs wfs) {
+    	 StaticGatewayProfile.wfs = wfs;
+    }
+    @RequestMapping("/setResources")
+    public void setResources (@RequestBody String res) {
+    	 StaticGatewayProfile.resources = res;
+    }
+    @RequestMapping("/setLocation")
+    public void setLocation (@RequestBody String loc) {
+    	StaticGatewayProfile.location = loc;
+    }
+    @RequestMapping("/setPricingPreferences")
+    public void setPricingPreferences (@RequestBody String pref) {
+    	StaticGatewayProfile.pricingPreferences = pref;
+    }
+    
+    
+    @RequestMapping("/setsupportedDataEncodingProtocolsOfGateway")
+    public void setsupportedDataEncodingProtocolsOfGateway (@RequestBody int[] prots ) {
+    	 StaticGatewayProfile.supportedDataEncodingProtocolsOfGateway = prots;
+    }
+    @RequestMapping("/setsupportedConnectivityProtocolsOfGateway")
+    public void setsupportedConnectivityProtocolsOfGateway (@RequestBody int[] prots ) {
+    	 StaticGatewayProfile.supportedConnectivityProtocolsOfGateway = prots;
+    }
+    @RequestMapping("/setuserRequirementWeight_Performance")
+    public void setuserRequirementWeight_Performance (@RequestBody int w ) {
+    	 StaticGatewayProfile.userRequirementWeight_Performance = w;
+    }
+    @RequestMapping("/setuserRequirementWeight_Reliability")
+    public void setuserRequirementWeight_Reliability (@RequestBody int w ) {
+    	 StaticGatewayProfile.userRequirementWeight_Reliability = w;
+    }
+    @RequestMapping("/setuserRequirementWeight_Cost")
+    public void setuserRequirementWeight_Cost (@RequestBody int w ) {
+    	 StaticGatewayProfile.userRequirementWeight_Cost = w;
+    }
+    
+    
 }
+
+
+
