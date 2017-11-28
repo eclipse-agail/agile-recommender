56e3274fdba9750702d0c3acdf6684b70cd86254
sedapolaterdeniz <spolater@ist.tugraz.at>
2017-11-28 15:33:51 +0100

    EPL v2

--- RecommenderAndConfigurator/src/main/java/org/eclipse/agail/recommenderandconfigurator/RnCAPI.java
+++ RecommenderAndConfigurator/src/main/java/org/eclipse/agail/recommenderandconfigurator/RnCAPI.java
@@ -1,14 +1,12 @@
-/*******************************************************************************
+/*********************************************************************
  * Copyright (C) 2017 TUGraz.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     TUGraz - initial API and implementation
- ******************************************************************************/
-
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
 package org.eclipse.agail.recommenderandconfigurator;
 
 
@@ -503,14 +501,12 @@ public class RnCAPI {
 	    	try{
 	    		System.out.println(mapper.writeValueAsString(recommenderProfile));	
 	    		restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
-	 		  
-	 		    System.out.println("settings succeeded");
-	 		    
 	    		result = restTemplate.postForObject(uri, recommenderProfile, ListOfWFs.class);
+	    		System.out.println("2- call the service done");
 	    	}catch(Exception e){
 	    		System.out.println("2- call the service failed: "+e.getMessage());
 	    	}
-	    	System.out.println("2- call the service done");
+	    	
     	}
 			
 		return result;
